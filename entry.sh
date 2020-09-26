b75e8195e06d73a4663c9a0dcaa8508d2e666a6a
eiaro <3652665+eiaro@users.noreply.github.com>
2020-09-26 03:01:09 +0200

    dgf

--- entry.sh
+++ entry.sh
@@ -14,7 +14,14 @@ bash "${STEAMCMD}" +login anonymous \
 if [ ! -f "${DATA_DIR}/default.ini" ]; then
     cp "${DATA_DIR}/rocketstation_DedicatedServer_Data/StreamingAssets/default.ini" ${DATA_DIR}
 
+    sed -i s/SERVERNAME=Stationeers/SERVERNAME=${SERVERNAME}/g ${DATA_DIR}/default.ini
+    sed -i s/GAMEPORT=27500/GAMEPORT=${GAMEPORT}/g ${DATA_DIR}/default.ini
+    sed -i s/UPDATERPORT=27015/UPDATERPORT=${UPDATERPORT}/g ${DATA_DIR}/default.ini
+    sed -i s/PASSWORD=/PASSWORD=${PASSWORD}/g ${DATA_DIR}/default.ini
+    sed -i s/ADMINPASSWORD=AdminPass/ADMINPASSWORD=${ADMINPASSWORD}/g ${DATA_DIR}/default.ini
+    sed -i s/MAPNAME=Moon/MAPNAME=${MAPNAME}/g ${DATA_DIR}/default.ini
+    sed -i s/DESCRIPTION=Stationeers/DESCRIPTION=${DESCRIPTION}/g ${DATA_DIR}/default.ini
+    sed -i s/MAXPLAYER=10/MAXPLAYER=${MAXPLAYER}/g ${DATA_DIR}/default.ini
     sed -i s/RCONPASSWORD=stationeers/RCONPASSWORD=${RCONPASSWORD}/g ${DATA_DIR}/default.ini
-     
 fi
 
