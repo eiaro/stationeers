aaf04b519d4cf1951d87eb6a46869797590e76c1
eiaro <3652665+eiaro@users.noreply.github.com>
2020-09-26 02:16:27 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -7,12 +7,12 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 
 # Assume missing config is a fresh install
 if [ ! -f "${STEAMAPPDIR}/default.ini" ]; then
-    cp $STEAMAPPDIR/rocketstation_DedicatedServer_Data/StreamingAssets/default.ini $STEAMAPPDIR
+    cp "${STEAMAPPDIR}/rocketstation_DedicatedServer_Data/StreamingAssets/default.ini" ${STEAMAPPDIR}
 
-    if [ -z $RCONPASSWORD ]; then
-        sed -i s/RCONPASSWORD=stationeers/RCONPASSWORD=$(pwgen 15 1)/g $STEAMAPPDIR/default.ini
+    if [ -z ${RCONPASSWORD} ]; then
+        sed -i s/RCONPASSWORD=stationeers/RCONPASSWORD=$(pwgen 15 1)/g ${STEAMAPPDIR}/default.ini
     else
-        sed -i s/RCONPASSWORD=stationeers/RCONPASSWORD=$RCONPASSWORD/g $STEAMAPPDIR/default.ini
+        sed -i s/RCONPASSWORD=stationeers/RCONPASSWORD=${RCONPASSWORD}/g ${STEAMAPPDIR}/default.ini
     fi 
 fi
 
